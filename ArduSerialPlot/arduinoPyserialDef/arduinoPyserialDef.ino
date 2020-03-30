1916e4c0fafcbc55bddf008e2ab28bc4dffec7cc
dainioshka <dainioshka@gmail.com>
2020-03-30 11:10:42 +0300

    add LCD_shield to arduinoPyserialDef

--- ArduSerialPlot/arduinoPyserialDef/arduinoPyserialDef.ino
+++ ArduSerialPlot/arduinoPyserialDef/arduinoPyserialDef.ino
@@ -1,16 +1,26 @@
 /*
  *If code initiated on arduino LCD_shield can take values analogVal to push a buttons.
- *Use with arduPyserialPlt.py.
+ *Use with arduPyserialPlot.py.
 */
+#include <LiquidCrystal.h>
+//use Arduino LCD_shield pins
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
 const int analogIn = 0;
 int analogVal = 0;
 
 void setup() {
 Serial.begin(9600);
+lcd.begin(16, 2);
+analogWrite(10, 30); //LCD_shield brightness
 }
 void loop() 
 {
 analogVal = analogRead(analogIn);
 Serial.println(analogVal);
+lcd.setCursor(0, 0);
+lcd.print("analogVal:> ");
+lcd.print(analogVal);
 delay(1000); 
+lcd.clear();
 }
